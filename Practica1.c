b4c2c2a31761b20cf300401e5e4ce30ed2487643
SOLISluis-manuel <solisss.personal.solisss@gmail.com>
2024-11-09 13:37:11 -0600

    Create Practica1.c

---
+++ Practica1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+
+
+void imprimir(char[]);
+
+int main ()
+{
+    char nombre[] = "Facultad de Ingenieria";
+    imprimir(nombre);
+}
+
+void imprimir(char s[])
+{
+    int tam;
+    for ( tam=strlen(s)-1 ; tam>=0 ; tam--)
+        printf("%c", s[tam]);
+    printf("\n");
+}
